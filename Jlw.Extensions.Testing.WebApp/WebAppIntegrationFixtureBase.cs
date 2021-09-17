5bb51b17161f112f303c547f9ba52affbf15d3d4
Jason Walker <jason@draxjinn.com>
2021-09-17 12:44:02 -0500

    Add project files.

---
+++ Jlw.Extensions.Testing.WebApp/WebAppIntegrationFixtureBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Extensions.Testing.WebApp
+{
+    [TestClass]
+    public abstract class WebAppIntegrationFixtureBase<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected HttpClient _client;
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing")
+                .UseStartup<TStartup>();
+
+            base.ConfigureWebHost(builder);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Testing")
+                .UseStartup<TStartup>();
+
+            _client = this.CreateClient();
+        }
+
+    }
+}
